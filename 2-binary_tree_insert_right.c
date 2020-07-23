c2de0f06061fda9da421fcc6d8e89f272c13fcaa
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-07-23 14:57:10 -0400

    add 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node as right-child of another node
+ * @parent: parent node
+ * @value: value of node
+ * Return: Node inserted
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+}
